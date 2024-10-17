a7791bdaf88b52927b6c0adc5b17bcac464e5ac4
Zepher <mohamedghiouan@gmail.com>
2024-10-17 17:58:04 +0100

    Updated Contact page and styles

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,9 +8,17 @@ function Contact() {
                 <h2>Contact</h2>
             </div>
             <div className="contact-media">
-                <div className='info-media'>
+                <div className='info-media d-flex flex-column'>
+                  <div className='d-flex flex-row'>
                     <span className='big'>Email</span>
                     <span className='small'>mohamedghiouan@gmail.com</span>
+                  </div>
+                  <div className='d-flex flex-row mt-2'>
+                    <span className='big'>Phone Number</span>
+                    <span className='small'>
+                      <a href="tel:+212656268304" target="_blank" rel="noreferrer">+212 65 62 68 304</a>
+                    </span>
+                  </div>
                 </div>
                 <div className='info-media'>
                     <span className='big'>Social Medias</span>
